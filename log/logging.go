3fbd352c2753879e6fd0b03d50ea786d18e85d2b
wuranxu <wuranxu@hellobike.com>
2021-01-14 16:23:54 +0800

    新增2种日志模式

--- log/logging.go
+++ log/logging.go
@@ -48,13 +48,17 @@ func (c *Log) Output(s string) {
 	c.file.WriteString(buf)
 }
 
-func InitLogger(filename string) *os.File {
+func InitLogger(filename string) *Log {
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
-	logger = &Log{file: file}
-	return file
+	return &Log{file: file}
+}
+
+func (c *Log) Info(v ...interface{}) {
+	c.SetPrefix(INFO)
+	c.Output(fmt.Sprintln(v...))
 }
 
 func Info(v ...interface{}) {
@@ -62,9 +66,19 @@ func Info(v ...interface{}) {
 	logger.Output(fmt.Sprintln(v...))
 }
 
+func (c *Log) Infof(format string, v ...interface{}) {
+	c.SetPrefix(INFO)
+	c.Output(fmt.Sprintf(format, v...))
+}
+
 func Infof(format string, v ...interface{}) {
 	logger.SetPrefix(INFO)
 	logger.Output(fmt.Sprintf(format, v...))
+}
+
+func (c *Log) Warn(v ...interface{}) {
+	c.SetPrefix(WARN)
+	c.Output(fmt.Sprintln(v...))
 }
 
 func Warn(v ...interface{}) {
@@ -72,9 +86,19 @@ func Warn(v ...interface{}) {
 	logger.Output(fmt.Sprintln(v...))
 }
 
+func (c *Log) Warnf(format string, v ...interface{}) {
+	c.SetPrefix(WARN)
+	c.Output(fmt.Sprintf(format, v...))
+}
+
 func Warnf(format string, v ...interface{}) {
 	logger.SetPrefix(WARN)
 	logger.Output(fmt.Sprintf(format, v...))
+}
+
+func (c *Log) Debug(v ...interface{}) {
+	c.SetPrefix(DEBUG)
+	c.Output(fmt.Sprintln(v...))
 }
 
 func Debug(v ...interface{}) {
@@ -82,14 +106,29 @@ func Debug(v ...interface{}) {
 	logger.Output(fmt.Sprintln(v...))
 }
 
+func (c *Log) Debugf(format string, v ...interface{}) {
+	c.SetPrefix(DEBUG)
+	c.Output(fmt.Sprintf(format, v...))
+}
+
 func Debugf(format string, v ...interface{}) {
 	logger.SetPrefix(DEBUG)
 	logger.Output(fmt.Sprintf(format, v...))
 }
 
+func (c *Log) Error(v ...interface{}) {
+	c.SetPrefix(ERROR)
+	c.Output(fmt.Sprintln(v...))
+}
+
 func Error(v ...interface{}) {
 	logger.SetPrefix(ERROR)
 	logger.Output(fmt.Sprintln(v...))
+}
+
+func (c *Log) Errorf(format string, v ...interface{}) {
+	c.SetPrefix(ERROR)
+	c.Output(fmt.Sprintf(format, v...))
 }
 
 func Errorf(format string, v ...interface{}) {
